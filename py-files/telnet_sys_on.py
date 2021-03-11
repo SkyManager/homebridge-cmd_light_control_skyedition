8df163c538ad5851b7d431a62214792568d4c355
SkyManager <vg@skyled.com.ua>
2021-03-11 08:36:58 +0200

    Create telnet_sys_on.py

---
+++ py-files/telnet_sys_on.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+import telnetlib
+import time
+from sys import argv
+
+host = argv[1]
+tn = telnetlib.Telnet(host, 1000)
+tn.write("sys on\n")
+#time.sleep(1)
+s = tn.read_until("\n")
+print s
+tn.close();
